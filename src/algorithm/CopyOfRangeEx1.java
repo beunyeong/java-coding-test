790d235c0109f732f8c359d74819663da2cf4381
beunyeong <beunyeong.b@gmail.com>
2026-01-05 11:44:20 +0900

    🔥study: Arrays.copyOfRange 범위 초과 및 문자열 배열 복사 예제 추가

--- src/algorithm/CopyOfRangeEx1.java
+++ src/algorithm/CopyOfRangeEx1.java
@@ -15,5 +15,20 @@ public class CopyOfRangeEx1 {
 
         int[] subArray1 = Arrays.copyOfRange(numbers, 0, 6);
         System.out.println(Arrays.toString(subArray1));
+
+
+        // 범위를 벗어난 경우
+        // 범위를 벗어나면 남은 값은 기본값으로 채워짐
+        // int -> 0
+        int[] subArray2 = Arrays.copyOfRange(numbers, 8, 12);
+        System.out.println(Arrays.toString(subArray2));
+
+
+        // 문자열 배열 복사
+        // String -> null
+        String[] fruits = {"Apple", "Banana"};
+
+        String[] subArray3 = Arrays.copyOfRange(fruits, 0, 3);
+        System.out.println(Arrays.toString(subArray3));
     }
 }
